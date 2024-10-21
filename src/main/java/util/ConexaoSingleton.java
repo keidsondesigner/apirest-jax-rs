8314680d595d9674c0c491ea53e534fda406948a
Keidson Roby <keidsondesigner@gmail.com>
2024-10-21 02:02:39 -0400

    chore: Atulização conexão com banco de dados

--- src/main/java/util/ConexaoSingleton.java
+++ src/main/java/util/ConexaoSingleton.java
@@ -8,20 +8,23 @@ public class ConexaoSingleton {
     private static Connection conexao = null;
     
     // Informações da conexão ao banco de dados
-    private static final String URL = "jdbc:mysql://localhost:3306/slq_impressionador";
+    private static final String URL = "jdbc:mysql://localhost:3306/base";
     private static final String USER = "root";
     private static final String PASSWORD = "3648Kk..";
 
     public static Connection getConnection() throws SQLException {
-        if (conexao == null || conexao.isClosed()) {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver"); // Carrega o driver do MySQL
-                conexao = DriverManager.getConnection(URL, USER, PASSWORD);
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
-                throw new SQLException("Erro ao conectar ao banco de dados");
-            }
+    if (conexao == null || conexao.isClosed()) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Tentando conectar ao banco de dados: " + URL);
+            conexao = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexão bem-sucedida!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            e.printStackTrace();
+            throw new SQLException("Erro ao conectar ao banco de dados", e);
         }
-        return conexao;
     }
+    return conexao;
+}
 }
