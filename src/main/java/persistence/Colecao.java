5374eaae5c3c250a93399fe8f0db698aa0f1b0ed
Keidson Roby <keidsondesigner@gmail.com>
2024-10-20 22:50:13 -0400

    feat: Criado Interface Colecao

---
+++ src/main/java/persistence/Colecao.java
@@ -0,0 +1,13 @@
+package persistence;
+
+import java.util.List;
+
+public interface Colecao<T> {
+
+  public List<T> todos() throws ColecaoException;
+  public T porId(int id) throws ColecaoException;
+  public void inserir(T objeto) throws ColecaoException;
+  public void alterar(T objeto) throws ColecaoException;
+  public void remover(T objeto) throws ColecaoException;
+  
+}
