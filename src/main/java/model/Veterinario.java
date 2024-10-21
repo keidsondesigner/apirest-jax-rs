d5904aa7d11206af5b8531ae4ef424b3ae64def0
Keidson Roby <keidsondesigner@gmail.com>
2024-10-20 22:49:57 -0400

    feat: Criado Model Veterinario

---
+++ src/main/java/model/Veterinario.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Veterinario {
+  private int id;
+  private String nome, crmv;
+  private double salario;
+
+  public Veterinario(int id, String nome, String crmv, double salario) {
+    this.id = id;
+    this.nome = nome;
+    this.crmv = crmv;
+    this.salario = salario;
+  }
+
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getCrmv() {
+    return crmv;
+  }
+  public void setCrmv(String crmv) {
+    this.crmv = crmv;
+  }
+  public double getSalario() {
+    return salario;
+  }
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
+
+  
+}
