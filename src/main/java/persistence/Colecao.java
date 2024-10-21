fc0aec3daf97cd287768a1c541dfba47e281e936
Keidson Roby <keidsondesigner@gmail.com>
2024-10-21 16:03:43 -0400

    chore: Atulizado Interface Colecao e ColecaoDeVeterinario

--- src/main/java/persistence/Colecao.java
+++ src/main/java/persistence/Colecao.java
@@ -7,6 +7,9 @@ public interface Colecao<T> {
 
   public List<T> todos() throws ColecaoException;
   public T porId(int id) throws ColecaoException;
+  public List<T> porNome(String nome) throws ColecaoException;
+  public T porCrmv(String crmv) throws ColecaoException;
+
   public void inserir(T objeto) throws ColecaoException;
   public void alterar(T objeto) throws ColecaoException;
   public void excluir(int id) throws ColecaoException;
--- src/main/java/persistence/ColecaoDeVeterinario.java
+++ src/main/java/persistence/ColecaoDeVeterinario.java
@@ -5,9 +5,13 @@ import exception.ColecaoException;
 import java.util.List;
 
 public interface ColecaoDeVeterinario extends Colecao<Veterinario> {
+
   public List<Veterinario> todos() throws ColecaoException;
   public Veterinario porId(int id) throws ColecaoException;
   public List<Veterinario> porNome(String nome) throws ColecaoException;
   public Veterinario porCrmv(String crmv) throws ColecaoException;
 
+  public void inserir(Veterinario veterinario) throws ColecaoException;
+  public void alterar(Veterinario veterinario) throws ColecaoException;
+  public void excluir(int id) throws ColecaoException;
 }
